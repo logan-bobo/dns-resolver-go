bf2643208199d00208612b13fe4edbfd00ea4bd1
Logan Cox <mail@logan-cox.com>
2023-12-11 07:44:18 +0000

    feat: generate hex of combined question and message and data structures to represent header, question and message

--- main.go
+++ main.go
@@ -1,20 +1,78 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"math/rand"
 	"strings"
 )
 
-// DNS question format as defined by RFC1035 4.1.2 -> https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
-type dnsQuestion struct {
-	QNAME  []byte
-	QTYPE  int //https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.2
-	QCLASS int //https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.4
-}
-
 // Static host for now to work out resolving logic, add ability for this to be passed in later
 const host string = "dns.google.com"
+
+// DNS question format as defined by RFC1035 4.1.2 -> https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
+type dnsQuestion struct {
+	qName  []byte
+	qType  uint16
+	qClass uint16
+}
+
+func (question *dnsQuestion) packQuestion() []byte {
+	var packedQuestion []byte
+
+	packedQuestion = append(packedQuestion, question.qName...)
+	packedQuestion = append(packedQuestion, uint16ToByteSlice(question.qType)...)
+	packedQuestion = append(packedQuestion, uint16ToByteSlice(question.qClass)...)
+
+	return packedQuestion
+}
+
+type dnsHeader struct {
+	id              uint16
+	flags           uint16
+	numQuestions    uint16
+	numAnswers      uint16
+	numAuthorityRR  uint16
+	numAdditionalRR uint16
+}
+
+func (header *dnsHeader) packHeader() []byte {
+	fieldsToPack := []uint16{header.id, header.flags, header.numQuestions, header.numAnswers, header.numAuthorityRR, header.numAdditionalRR}
+	packedFields := packuint16Fields(fieldsToPack)
+	return packedFields
+}
+
+type dnsMessage struct {
+	header   dnsHeader
+	question dnsQuestion
+}
+
+func (message *dnsMessage) packMessage(header[]byte, dnsQuestion[]byte) []byte {
+	var packedMessage []byte
+
+	packedMessage = append(packedMessage, header...)
+	packedMessage = append(packedMessage, dnsQuestion...)
+
+	return packedMessage
+}
+
+func (message *dnsMessage) generateHex(bytes []byte) string {
+	var hexString string
+	
+	hexString = hex.EncodeToString(bytes)
+
+	return hexString
+}
+
+func packuint16Fields (fields []uint16) []byte {
+	var packedFields []byte
+	
+	for _, field := range fields {
+		packedFields = append(packedFields, uint16ToByteSlice(field)...)
+	}
+
+	return packedFields
+}
 
 func generateQueryID() int {
 	// 65535 is the largest number we can represent in 16bits (=2^16–1)
@@ -38,21 +96,44 @@ func encodeHost(host string) []byte {
 	return result
 }
 
+func uint16ToByteSlice(number uint16) []byte {
+	// uint16 is represented in 2 bytes
+	slice := make([]byte, 2)
+	slice[0] = byte(number >> 8)
+	slice[1] = byte(number)
+
+	return slice
+}
+
 func main() {
-	// Generate query ID that must be represented in 16bits defined by ID - https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
-	queryId := generateQueryID()
-	fmt.Println("Query ID:", queryId)
-
-	// Encode the hostname to standard defined by QNAME - https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
-	encodedHost := encodeHost(host)
-	fmt.Println(encodedHost)
-
-	// Build out a static question for now
+	// Build out a static question for now, can be dynamic later...
 	sendingQuestion := dnsQuestion{
-		QNAME:  encodedHost, 
-		QTYPE:  1, // A Record
-		QCLASS: 1, // INTERNET
+		qName:  encodeHost(host),
+		qType:  1, // A Record
+		qClass: 1, // INTERNET
 	}
 
-	fmt.Println(sendingQuestion.QCLASS, sendingQuestion.QNAME, sendingQuestion.QTYPE)
+	// Build out a static message, can be dynamic later...
+	sendingHeader := dnsHeader{
+		id:              22, // static for now but will need to be unique later call generateQueryID()
+		flags:           256, // Setting the 'recursion desired' bit to 1 (8th bit) in uint16
+		numQuestions:    1,
+		numAnswers:      0,
+		numAuthorityRR:  0,
+		numAdditionalRR: 0,
+	}
+	
+	sendingMessage := dnsMessage{
+		header: sendingHeader,
+		question: sendingQuestion,
+	}
+
+	message := sendingMessage.packMessage(
+		sendingHeader.packHeader(),
+		sendingQuestion.packQuestion(),
+	)
+
+	hex := sendingMessage.generateHex(message)
+
+	fmt.Println(hex)
 }
