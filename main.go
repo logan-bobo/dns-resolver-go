7156a1880fb635605311023c5ec4f929330b0498
Logan <mail@logan-cox.com>
2023-12-15 14:18:43 +0000

    feat: pasre respose header from upstream DNS server (#2)
    * feat: parse response message and display IPv4 and return domain

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log"
@@ -66,6 +67,18 @@ func (message *dnsMessage) generateHex(bytes []byte) string {
 	return hexString
 }
 
+// https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
+type resourceRecord struct {
+	// name is a compressed field see - https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.4
+	name        string
+	recordType  uint16
+	recordClass uint16
+	// This will be how long we want to hold this data in memory for at some point so repeated resolutions do not do an upstream expensive network call
+	TTL      uint32
+	RDLength uint16
+	RDData   net.IP
+}
+
 func packuint16Fields(fields []uint16) []byte {
 	var packedFields []byte
 
@@ -98,6 +111,33 @@ func encodeHost(host string) []byte {
 	return result
 }
 
+func decodeHost(encodedHost []byte) string {
+	var valueShifts []int
+	var domainParts []string
+
+	shift := encodedHost[0]
+	fqdnBytes := 0
+
+	for shift != 0 {
+		valueShifts = append(valueShifts, int(shift))
+		fqdnBytes += int(shift) + 1
+		shift = encodedHost[fqdnBytes]
+	}
+
+	sliceStart := 1
+
+	for _, valueShift := range valueShifts {
+		domainParts = append(domainParts, string(encodedHost[sliceStart:valueShift+sliceStart]))
+		sliceStart = sliceStart + valueShift + 1
+	}
+
+	return strings.Join(domainParts, ".")
+}
+
+func decodeIP(IP []byte) net.IP {
+	return net.IPv4(IP[0], IP[1], IP[2], IP[3])
+}
+
 func uint16ToByteSlice(number uint16) []byte {
 	// uint16 is represented in 2 bytes
 	slice := make([]byte, 2)
@@ -105,6 +145,103 @@ func uint16ToByteSlice(number uint16) []byte {
 	slice[1] = byte(number)
 
 	return slice
+}
+
+func bytesToUint16(bytes []byte) uint16 {
+	if len(bytes) != 2 {
+		panic("Slice of invalid length passed to function, can not convert to uint16")
+	}
+
+	return uint16(bytes[0])<<8 | uint16(bytes[1])
+}
+
+// split this out
+func unpackReturnMessage(message []byte) []resourceRecord {
+	// The header is the first 12 bytes
+	headerbytes := message[:12]
+
+	returnHeader := dnsHeader{
+		id:              bytesToUint16(headerbytes[:2]),
+		flags:           bytesToUint16(headerbytes[2:4]),
+		numQuestions:    bytesToUint16(headerbytes[4:6]),
+		numAnswers:      bytesToUint16(headerbytes[6:8]),
+		numAuthorityRR:  bytesToUint16(headerbytes[8:10]),
+		numAdditionalRR: bytesToUint16(headerbytes[10:12]),
+	}
+
+	// check QR bit is set from flags it will be the first bit in the 16bits that make up flags
+	// we know if flags is larger than 2^15-1 the first bit is set (I think...) as 2^15-1 == 32767
+	if returnHeader.flags < 32767 {
+		panic("No response from DNS server")
+	}
+
+	// The question is the encoded host we sent so to work out the question bytes in the response its the length of our initial question + 4
+	// as a domain must always end with a 0 padding byte to indicate the end of the domain for example
+	// 3dns6google3com0 -> [3 100 110 115 6 103 111 111 103 108 101 3 99 111 109 0] we can find the first 0 byte at the end of the domain
+	returnQuestionInitial := message[12:]
+	shift := returnQuestionInitial[0]
+	questionBytes := 0
+
+	for shift != 0 {
+		questionBytes += int(shift) + 1
+		shift = returnQuestionInitial[questionBytes]
+	}
+
+	questionBytes += 5 // We need to add the 0 padding byte at the end of the domain and the 4 extra bytes
+
+	initalAnswer := message[12+questionBytes:]
+
+	var answers [][]byte
+
+	seperator := len(initalAnswer)/int(returnHeader.numAnswers) - 1
+	count := 0
+	part := 0
+
+	for index := range initalAnswer {
+		if count == seperator {
+			answers = append(answers, initalAnswer[part:index+1])
+			count = 0
+			part += seperator + 1
+			continue
+		}
+
+		count += 1
+	}
+
+	var resourceRecords []resourceRecord
+
+	for _, answer := range answers {
+
+		// If the number is bigger than 49152 we know the first two bits are set and the message is compressed.
+		if binary.BigEndian.Uint16(answer[0:2]) > 49152 {
+			referenceDomain := binary.BigEndian.Uint16(answer[0:2]) - 49152 // The shift of 8bit (bytes) from start of the message the source domain this is our pointer.
+
+			fqdninitial := message[referenceDomain:]
+
+			shift := fqdninitial[0]
+			fqdnBytes := 0
+
+			for shift != 0 {
+				fqdnBytes += int(shift) + 1
+				shift = returnQuestionInitial[fqdnBytes]
+			}
+
+			fqdn := message[referenceDomain : (int(referenceDomain)+int(fqdnBytes))+1]
+
+			answerResourceRecord := resourceRecord{
+				name:        decodeHost(fqdn),
+				recordType:  binary.BigEndian.Uint16(answer[2:4]),
+				recordClass: binary.BigEndian.Uint16(answer[4:6]),
+				TTL:         binary.BigEndian.Uint32(answer[6:10]),
+				RDLength:    binary.BigEndian.Uint16(answer[10:12]),
+				RDData:      decodeIP(answer[12 : 12+binary.BigEndian.Uint16(answer[10:12])]),
+			}
+
+			resourceRecords = append(resourceRecords, answerResourceRecord)
+		}
+	}
+
+	return resourceRecords
 }
 
 func sendMessage(message []byte) []byte {
@@ -163,9 +300,12 @@ func main() {
 		sendingQuestion.packQuestion(),
 	)
 
-	fmt.Println("Sending bytes:", message)
-
 	response := sendMessage(message)
 
-	fmt.Println("Response bytes:", response)
+	unpackedResponses := unpackReturnMessage(response)
+
+	for _, unpackedResponse := range unpackedResponses {
+		fmt.Println("Domain -", unpackedResponse.name, "IPv4 -", unpackedResponse.RDData)
+	}
+
 }
